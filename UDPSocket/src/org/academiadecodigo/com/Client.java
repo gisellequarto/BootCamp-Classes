7061e25670338b9a44a2be58b2596f33d500c0aa
gisellequarto <gisellequarto@hotmail.com>
2020-10-29 14:44:17 +0000

    finish udp connection

--- UDPSocket/src/org/academiadecodigo/com/Client.java
+++ UDPSocket/src/org/academiadecodigo/com/Client.java
@@ -2,32 +2,70 @@ package org.academiadecodigo.com;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.Scanner;
 
 public class Client {
 
+    private static DatagramSocket socket;
+    private static InetAddress host;
 
-    public static void sendPack(String message) throws UnknownHostException {
-        byte[] sendBuffer = message.getBytes();
+
+    public static void openSocket() {
         try {
-            DatagramSocket socket = new DatagramSocket(9999);
-            DatagramPacket sendPack = new DatagramPacket(sendBuffer, sendBuffer.length, InetAddress.getByName("ubuntugi") , 8080);
-            socket.send(sendPack);
-            socket.close();
+            socket = new DatagramSocket();
         } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public static void sendPack(String message) {
+        byte[] sendBuffer = message.getBytes();
+
+        try {
+            host = InetAddress.getLocalHost();
+            DatagramPacket sendPack = new DatagramPacket(sendBuffer, sendBuffer.length, host, 8080);
+            socket.send(sendPack);
+        } catch (UnknownHostException e) {
+            System.err.println("Host we have a problem");
+            ;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void receivePack() {
+        byte[] recBuffer = new byte[2048];
+        try {
+            DatagramPacket receivePack = new DatagramPacket(recBuffer, recBuffer.length);
+            socket.receive(receivePack);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String message = new String(recBuffer);
+        System.out.println(message);
+    }
+
+    public static void closeSocket() {
+        socket.close();
+    }
 
 
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) {
 
-        String message = "hello";
+        Scanner scanner = new Scanner(System.in);
+            String message = scanner.nextLine();
 
-        sendPack(message);
 
+        openSocket();
+
+        while(message != " ") {
+            sendPack(message);
+            receivePack();
+            message = scanner.nextLine();
+        }
+
+        closeSocket();
 
     }
 
--- UDPSocket/src/org/academiadecodigo/com/Server.java
+++ UDPSocket/src/org/academiadecodigo/com/Server.java
@@ -3,32 +3,64 @@ package org.academiadecodigo.com;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 
 public class Server {
 
+    private static DatagramSocket socket;
+    private static DatagramPacket pack;
+    private static String message;
+
+    public static void openSocket() {
+        try {
+            socket = new DatagramSocket(8080);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void receivePack() {
         byte[] recBuffer = new byte[2048];
+        pack = new DatagramPacket(recBuffer, recBuffer.length);
+
         try {
-            DatagramSocket socket = new DatagramSocket(8080);
-            DatagramPacket receivePack = new DatagramPacket(recBuffer, recBuffer.length);
-            socket.receive(receivePack);
-            socket.close();
-        } catch (SocketException e) {
-            e.printStackTrace();
+            socket.receive(pack);
+            message = (new String(recBuffer));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String message = new String(recBuffer);
-        System.out.println(message);
     }
 
+
+    public static void sendUpperCasePack() {
+        message = message.toUpperCase();
+        byte[] sendBuffer = message.getBytes();
+        DatagramPacket sendPack = new DatagramPacket(sendBuffer, sendBuffer.length, pack.getAddress(), pack.getPort());
+
+        try {
+            socket.send(sendPack);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeSocket() {
+        socket.close();
+    }
 
 
     public static void main(String[] args) {
 
-        receivePack();
+        openSocket();
+
+
+        while (!socket.isClosed()) {
+            receivePack();
+            sendUpperCasePack();
+        }
+
+        closeSocket();
 
     }
 }
